5d41487ff7cfd5e7a2036f7b110a5294690b4801
Abhishek Kumar <abhishekkuma@enphaseenergy.com>
2025-12-29 18:08:19 +0530

    Pagination, Prefetching And Mutations

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # udemy-REACT-QUERY
 
 Code to support the Udemy course [React Query: Server State Management in React](https://www.udemy.com/course/learn-react-query/?couponCode=REACT-QUERY-GITHUB)
+
+# React Query Docs
+
+https://tanstack.com/query/latest/docs/framework/react/overview
+
+# Use Query
+
+(https://tanstack.com/query/latest/docs/framework/react/reference/useQuery)
+
+# Query Retries
+
+(https://tanstack.com/query/latest/docs/framework/react/guides/query-retries)
+
+# Devtools
+
+(https://tanstack.com/query/latest/docs/framework/react/devtools)
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,8 +9,43 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const queryClient = useQueryClient();
+
+  const deleteMutation = useMutation({
+    mutationFn: (postId) => {
+      deletePost(postId);
+    },
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: (postId) => {
+      updatePost(postId);
+    },
+  });
+
+  // deleteMutation.mutate
+
+  useEffect(() => {
+    const nextPage = currentPage + 1;
+    queryClient.prefetchQuery({
+      queryKey: ["posts", nextPage],
+      queryFn: () => fetchPosts(nextPage),
+    });
+  }, [currentPage, queryClient]);
+
+  const { data, isError, isLoading, error } = useQuery({
+    queryKey: ["posts", currentPage],
+    queryFn: () => fetchPosts(currentPage),
+    staleTime: 2000, // 2 seconds
+  });
+
+  if (isLoading) {
+    return <h3>Loading posts...</h3>;
+  }
+
+  if (isError) {
+    return <div>OOPS {error.toString()}</div>;
+  }
 
   return (
     <>
@@ -18,23 +54,42 @@ export function Posts() {
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
+            onClick={() => {
+              deleteMutation.reset();
+              setSelectedPost(post);
+            }}
           >
             {post.title}
           </li>
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
       <hr />
-      {selectedPost && <PostDetail post={selectedPost} />}
+      {selectedPost && (
+        <PostDetail
+          post={selectedPost}
+          deleteMutation={deleteMutation}
+          updateMutation={updateMutation}
+        />
+      )}
     </>
   );
 }
